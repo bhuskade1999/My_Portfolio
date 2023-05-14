b338fdd8f56a1762a4cd687d1556bad088205364
bhushan <bbhuskade123@gmail.com>
2023-05-15 00:29:45 +0530

    backend commit

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,10 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 export const app = express();
+const cors = require("cors")
 
+
+app.use(cors())
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
